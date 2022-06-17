09b59a873587c622d391d25600e26b4b4381283e
adrianoff <adrianov87@gmail.com>
2022-06-17 19:20:25 +0300

    forms and inputs

--- src/src/Components/Form.jsx
+++ src/src/Components/Form.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import MyButton from "./UI/button/MyButton";
 import MyInput from "./UI/input/MyInput";
 
 const Form = () => {
+
+    const [title, setTitle] = useState('')
+    const bodyInputRef = useRef()
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        console.log(title)
+        console.log(bodyInputRef.current.value)
+    }
+
     return (
         <div>
             <form>
-                <MyInput type="text" placeholder="Title"/>
-                <MyInput type="text" placeholder="Content"/>
-                <MyButton disabled>Save</MyButton>
+                <MyInput
+                    onChange={e => setTitle(e.target.value)}
+                    value={title} type="text" placeholder="Title"
+                />
+                <MyInput
+                    ref={bodyInputRef}
+                    type="text" placeholder="Content"
+                />
+                <MyButton onClick={addNewPost}>Save</MyButton>
             </form>
         </div>
     );
