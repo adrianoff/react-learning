6cc70383833bbee418e4b149d6e429c9e1e5afd6
adrianoff <adrianov87@gmail.com>
2022-06-19 14:58:18 +0300

    forms and inputs

--- src/src/App.js
+++ src/src/App.js
@@ -3,6 +3,9 @@ import Counter from "./Components/Counter";
 import './Styles/App.css'
 import PostList from "./Components/PostList";
 import Form from "./Components/Form";
+import MyInput from "./Components/UI/input/MyInput";
+import MyTextArea from "./Components/UI/textarea/MyTextArea";
+import MyButton from "./Components/UI/button/MyButton";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -10,10 +13,36 @@ function App() {
         {id: 2, title: 'Post title', body: 'Post body'},
         {id: 3, title: 'Post title', body: 'Post body'}
     ])
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        const newPost = {
+            id: Date.now(),
+            title,
+            content
+        }
+        setPosts([...posts, newPost])
+        setTitle('')
+        setContent('')
+    }
 
     return (
         <div className="App">
-            <Form />
+            {/*<Form />*/}
+            <form>
+                <MyInput
+                    onChange={e => setTitle(e.target.value)}
+                    value={title} type="text" placeholder="Title"
+                />
+                <MyTextArea
+                    onChange={e => setContent(e.target.value)}
+                    value={content}
+                    placeholder="Content"
+                />
+                <MyButton onClick={addNewPost}>Save</MyButton>
+            </form>
             <PostList posts={posts} title="Posts List" />
 
             {/*<Counter/>*/}
